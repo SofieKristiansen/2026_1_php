8bedec116fb24c364dafd4ceca1af2ac0a33d6fc
Sofie Gamborg Kristiansen <158562639+SofieKristiansen@users.noreply.github.com>
2026-02-23 10:15:03 +0100

    Opdater markører til at hente data fra PHP properties

--- _main.php
+++ _main.php
@@ -1,5 +1,8 @@
-<main>
+<?php
+require_once __DIR__ . "/properties.php";
+?>
 
+<main>
     <div id="map">Map</div>
 
 <!-- Leaflet JS -->
@@ -18,36 +21,20 @@
         maxZoom: 20
     }).addTo(map);
 
-        // Add a marker
-        let marker = L.marker([55.67960020013266, 12.56464935119663], {
-            icon: L.divIcon({
-                className: '', // removes default marker styles
-                html: `
-                    <button class="marker" mix-get="api-get-item?item_pk=1"></button>
-                `,
-                iconSize: [20, 20],      // size of button
-                iconAnchor: [20, 20]      // center it properly
-            }),            
-            item_pk: "1"
-        }).addTo(map);
-        marker.on('click', function (e) {
-        }); 
-        
-        
-        marker = L.marker([55.6899549465982, 12.526712188294855], {
-            icon: L.divIcon({
-                className: '', // removes default marker styles
-                html: `
-                    <button class="marker" mix-get="api-get-item?item_pk=2"></button>
-                `,
-                iconSize: [20, 20],      // size of button
-                iconAnchor: [20, 20]      // center it properly
-            }),             
-            item_pk: "2"
-        }).addTo(map);
-        marker.on('click', function (e) {
-        });      
+    // Hent properties fra PHP
+    const properties = <?php echo json_encode($properties); ?>;
 
+    properties.forEach(property => {
+        const iconHtml = `<button class="marker ${property.type}" mix-get="api-get-item?item_pk=${property.id}"></button>`;
+        
+        L.marker([property.lat, property.lng], {
+            icon: L.divIcon({
+                className: '', 
+                html: iconHtml
+            })
+        }).addTo(map);
+    });
+</script>
 
 </script>
 
