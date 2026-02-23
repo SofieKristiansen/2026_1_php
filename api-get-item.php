5b3bbaf819892568a171ff18864e096eeb3d2f51
Sofie Gamborg Kristiansen <158562639+SofieKristiansen@users.noreply.github.com>
2026-02-23 10:14:46 +0100

    Opdateret api-get-item til at vise type, pris og billede fra properties

--- api-get-item.php
+++ api-get-item.php
@@ -1,10 +1,18 @@
 <browser mix-update="aside">
+<?php
+require_once __DIR__ . "/properties.php";
+$item_id = $_GET['item_pk'] ?? null;
 
-    <section>
-        <div><?= $_GET["item_pk"] ?></div>
-        <div>Name</div>
-        <div>Price</div>
-    </section>
-
+foreach ($properties as $property) {
+    if ($property['id'] == $item_id) {
+        ?>
+        <section class="property">
+            <div>Type: <?= $property['name'] ?></div>
+            <div>Price: <?= $property['price'] ?></div>
+            <img src="<?= $property['image'] ?>" alt="<?= $property['type'] ?>" width="200px">
+        </section>
+        <?php
+    }
+}
+?>
 </browser>
-
